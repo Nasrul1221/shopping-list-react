f0b09ba25d82b6dc0092e1b2690c3c28fed220d1
nasrul1221 <fst20071221@gmail.com>
2025-05-02 22:09:25 +0300

    Summary objects in a cart

--- src/summary-side/SummarySide.jsx
+++ src/summary-side/SummarySide.jsx
@@ -3,20 +3,26 @@ import './styles.css';
 import {CartContext} from "../CardProvider.jsx";
 
 function SummarySide() {
-    const [productQuantity, setProductQuantity] = React.useState(0);
+    const [productList, setProductList] = React.useState([]);
     const productsInCart = useContext(CartContext).productsInCart;
 
     React.useEffect(() => {
-        productsInCart.forEach(() => {
-            setProductQuantity(productQuantity + 1);
+        const list = productsInCart.map((product) => {
+            return (
+                <div className={'summary-object'}>
+                    <img src={product.image} alt={product.name} className={'summary-image'} />
+                    <h2 className={'summary-object-title'}>{product.title}</h2>
+                </div>
+            )
         })
+        setProductList(list);
     }, [productsInCart]);
 
     return (
         <div className={'SummarySide'}>
             <h1>Cart</h1>
             <div>
-                <p>Quantity: {productQuantity}</p>
+                {productList}
             </div>
         </div>
     )
